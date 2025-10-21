605a97392f081e0995215f62f0469b48fc6763be
andreadelaroca <asodelaroca@uamv.edu.ni>
2025-10-21 15:04:09 -0600

    Ejercicio terminado

--- caso1/Program.cs
+++ caso1/Program.cs
@@ -5,35 +5,39 @@ class Program
     public static Stack<string> acciones = new Stack<string>();
     static void Main()
     {
-        Console.WriteLine("-----Menú de acciones-----");
-        Console.WriteLine("1. Agregar acción");
-        Console.WriteLine("2. Visualizar última acción");
-        Console.WriteLine("3. Deshacer última acción");
-        Console.WriteLine("4. Salir");
-        Console.Write("Elija una opción: ");
-        int opcion = Convert.ToInt32(Console.ReadLine());
-
-        switch (opcion)
+  
+        while (true)
         {
-            case 1:
-                AgregarAccion();
-                break;
+            Console.WriteLine("-----Menú de acciones-----");
+            Console.WriteLine("1. Agregar acción");
+            Console.WriteLine("2. Visualizar última acción");
+            Console.WriteLine("3. Deshacer última acción");
+            Console.WriteLine("4. Salir");
+            Console.Write("Elija una opción: ");
+            int opcion = Convert.ToInt32(Console.ReadLine());
+        
+            switch (opcion)
+            {
+                case 1:
+                    AgregarAccion();
+                    break;
 
-            case 2:
-                VisualizarAccion();
-                break;
+                case 2:
+                    VisualizarAccion();
+                    break;
 
-            case 3:
-                DeshacerAccion();
-                break;
+                case 3:
+                    DeshacerAccion();
+                    break;
 
-            case 4:
-                break;
+                case 4:
+                    return;
 
-            default:
-                Console.WriteLine("Opción no válida");
-                break;
-        }
+                default:
+                    Console.WriteLine("Opción no válida");
+                    break;
+            }
+        } 
     }
     static void AgregarAccion()
     {
@@ -42,10 +46,25 @@ class Program
     }
     static void VisualizarAccion()
     {
-
+        if (acciones.Count == 0)
+        {
+            Console.WriteLine("No hay acciones registradas");
+        }
+        else
+        {
+            Console.WriteLine($"La última acción fue: {acciones.Peek()}");
+        }
     }
     static void DeshacerAccion()
     {
-
+        if (acciones.Count == 0)
+        {
+            Console.WriteLine("No hay acciones registradas");
+        }
+        else
+        {
+            Console.WriteLine($"La acción {acciones.Peek()} será desecha.");
+            acciones.Pop();
+        }
     }
 }
--- caso1/obj/Debug/net9.0/caso1.AssemblyInfo.cs
+++ caso1/obj/Debug/net9.0/caso1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("caso1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36ec27d2aeb64c583b4277cbc6b0886c2b4dcc9a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2978d8f8b8264fd0ac67935b40ec42527a242e4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("caso1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("caso1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
