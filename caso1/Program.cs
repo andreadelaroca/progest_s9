36ec27d2aeb64c583b4277cbc6b0886c2b4dcc9a
andreadelaroca <asodelaroca@uamv.edu.ni>
2025-10-21 11:43:06 -0600

    Opciones del programa

--- caso1/Program.cs
+++ caso1/Program.cs
@@ -2,8 +2,49 @@
 
 class Program
 {
+    public Stack<string> acciones = new Stack<string>();
     static void Main()
     {
-        
+        Console.WriteLine("-----Menú de acciones-----");
+        Console.WriteLine("1. Agregar acción");
+        Console.WriteLine("2. Visualizar última acción");
+        Console.WriteLine("3. Deshacer última acción");
+        Console.WriteLine("4. Salir");
+        Console.Write("¿Qué desea hacer?");
+        int opcion = Convert.ToInt32(Console.ReadLine());
+
+        switch (opcion)
+        {
+            case 1:
+                AgregarAccion();
+                break;
+
+            case 2:
+                VisualizarAccion();
+                break;
+
+            case 3:
+                DeshacerAccion();
+                break;
+
+            case 4:
+                break;
+
+            default:
+                Console.WriteLine("Opción no válida");
+                break;
+        }
+    }
+    static void AgregarAccion()
+    {
+
+    }
+    static void VisualizarAccion()
+    {
+
+    }
+    static void DeshacerAccion()
+    {
+
     }
 }
